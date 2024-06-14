e5ce9e68dccf7c435e5232089c4f2cbba39ae5d4
Kaying <39971215+Kay-Chu@users.noreply.github.com>
2024-06-15 00:47:20 +0800

    handled CORS

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,14 @@
+import { Typography } from 'antd';
+import React from 'react';
+
+const { Text } = Typography;
+
+const Copyright = () => {
+    return (
+        <>
+            <Text>(c) 2024 Web API Development of demonstration ONLY</Text>
+        </>
+    )
+}
+
+export default Copyright
